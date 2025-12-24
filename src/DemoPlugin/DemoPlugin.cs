e436aaf7d4b35c9c70c81178204e9d3b136fd5cd
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2025-12-24 18:38:14 +0300

    v2

--- src/DemoPlugin/DemoPlugin.cs
+++ src/DemoPlugin/DemoPlugin.cs
@@ -1,22 +1,56 @@
 using PluginManager.Api;
+using PluginManager.Api.Capabilities.Implementations.Commands;
+using PluginManager.Api.Capabilities.Implementations.Events.GameEvents;
+using PluginManager.Api.Contracts;
+using PluginManager.Api.Hooks;
 
 namespace DemoPlugin;
 
-public class DemoPlugin : IPlugin
+public class DemoPlugin : BasePlugin
 {
-    public string ModuleName => "DemoPlugin";
-    public string ModuleVersion => "1.0.0";
-    public string ModuleAuthor => "TouchMe-Inc";
-    public string ModuleDescription => "Demo plugin";
-    public string ModulePath { get; set; }
+    public override string ModuleName => "DemoPlugin";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "TouchMe-Inc";
+    public override string ModuleDescription => "Demo plugin";
 
-    public void Load(bool hotReload)
+    protected override void OnLoad()
     {
-        Log.Out($"[{ModuleName}] Loaded (hotReload={hotReload}) from {ModulePath}");
+        RegisterEventHandler<ChatMessageEvent>(OnChatMessage, HookMode.Pre);
+        RegisterEventHandler<PlayerJoinedGameEvent>(OnPlayerJoined, HookMode.Pre);
+        RegisterEventHandler<TileEntityAccessAttemptEvent>(OnTileEntityAccessAttempt, HookMode.Pre);
+
+        RegisterCommand("help", "Print demo help", OnTriggeredHelp);
+
+        Log.Out($"Plugin {ModuleName} is loaded");
     }
 
-    public void Unload(bool hotReload)
+    protected override void OnUnload()
     {
-        Log.Out($"[{ModuleName}] Unloaded (hotReload={hotReload})");
+        Log.Out($"Plugin {ModuleName} is unloaded");
     }
+
+    private HookResult OnChatMessage(ChatMessageEvent evt)
+    {
+        evt.Message = "[00ff00]" + evt.Message;
+        evt.BBMode = BbCodeSupportMode.Supported;
+
+        return HookResult.Changed;
+    }
+
+    private HookResult OnPlayerJoined(PlayerJoinedGameEvent evt)
+    {
+        Log.Out($"{evt.EventName} : {evt.EntityId}");
+        return HookResult.Continue;
+    }
+
+    private HookResult OnTileEntityAccessAttempt(TileEntityAccessAttemptEvent evt)
+    {
+        Log.Out($"{evt.EntityIdThatOpenedIt} : {evt.TileEntityType}");
+
+        return HookResult.Continue;
+    }
+
+    private void OnTriggeredHelp(ICommandContext ctx) =>
+        ChatMessenger.SendTo(ctx.EntityId,
+            $"[ffaaaa][{ModuleName}][-] Client #{ctx.EntityId} trigger help");
 }
